e2e5968588b0fa2e96c82513dcca9de8bb19f9c6
vineferreira <113901271+vineferreira@users.noreply.github.com>
2024-03-26 12:35:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 const botoes=document.querySelectorAll(".botao");
+const textos=document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i<botoes.length;i++){
+for(let i= 0;i < botoes.length;i++){
    botoes[i].onclick = function(){
-       for(let j=0;j < botoes.length;j++){
+       for(let j= 0;j < botoes.length;j++){
            botoes[j].classList.remove("ativo");
+           textos[j].classList.remove("ativo");
        }
        botoes[i].classList.add("ativo");
+       textos[i].classList.add("ativo");
    }
 }
 
+const contadores=document.querySelectorAll(".contador");
+const tempoObjetivo1=new Date("2024-03-26T12:10:00");
+let tempoAtual =  new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calcularTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor/(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %+24;
+}
 
 
 
